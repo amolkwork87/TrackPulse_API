388e7fa3004c8aeed2a9f14493ee53942c673df2
amolkwork87 <amolk.work.87@gmail.com>
2026-05-16 11:28:20 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -123,7 +123,7 @@ app.UseStaticFiles(new StaticFileOptions
 app.MapControllers();
 
 app.Run();
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
+ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+ {
+     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+ }
